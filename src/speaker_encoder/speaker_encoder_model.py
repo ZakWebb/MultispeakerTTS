cc09112e2005eb16d919ad40082afe54e54872e5
Zak Webb <zakwwebb@gmail.com>
2020-02-09 21:57:02 -0800

    Added Resnet Model for speaker embedding

--- src/speaker_encoder/speaker_encoder_model.py
+++ src/speaker_encoder/speaker_encoder_model.py
@@ -11,33 +11,30 @@ num_LSTM_layers = 3
 
 def get_model():
     tf.keras.backend.set_floatx('float64')
-    speaker_encoder_model = tf.keras.Sequential()
-    for layer in range(num_LSTM_layers):
-        speaker_encoder_model.add(layers.LSTM(hidden_layer_dim,
-                                              name='LSTM %d' % (layer),
-                                              return_sequences=not (layer == num_LSTM_layers - 1)))
-        speaker_encoder_model.add(layers.Dense(projection_layer_dim, name='Projection %d' % (layer)))
 
+    return SpeakerEmbedding()
+
+
+def get_LSTM_layers():
+    speaker_encoder_model = []
+    for layer in range(num_LSTM_layers):
+        speaker_encoder_model.append(layers.LSTM(hidden_layer_dim,
+                                                 name='LSTM %d' % (layer),
+                                                 return_sequences=not (layer == num_LSTM_layers - 1)))
+        speaker_encoder_model.append(layers.Dense(projection_layer_dim, name='Projection %d' % (layer)))
     return speaker_encoder_model
 
 
-class TDNNBlock(layers.Layer):
-    def __init__(self):
-        super(TDNNBlock, self, **kwargs).__init__(**kwargs)
+class SpeakerEmbedding(tf.Model):
+    def __init__(**kwargs):
+        super(SpeakerEmbedding, self).__init__(**kwargs)
 
-        tdnn_schedule = [(5, 1), (3, 2), (3, 3)]
-        self.conv_layers = []
-        for size, dilation in tdnn_schedule:
-            self.conv_layers.append(layers.Conv1D(512, size, dilation=dilation, activation='relu', use_bias=False))
+        # Define the encoding model for each frame
+        self.layers = get_LSTM_layers()
 
-        self.dropout_rate = 0.15
-        self.dropout = layers.Dropout(rate=self.dropout_rate)
-
-    def call(self, inputs, training=False, ):
+    def call(self, inputs):
         x = inputs
-        for conv_layer in self.conv_layers:
-            x = conv_layer(x)
-            if training:
-                x = self.dropout(x)
+        for layer in self.layers:
+            x = layer(x)
 
         return x

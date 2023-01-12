9f503514ab36ed447fffb799ada8b4e48edc494b
Zak Webb <zakwwebb@gmail.com>
2023-01-11 20:57:28 -0800

    Starting HiFi-GAN vocoder

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Github References:
 - [xcmyz/FastSpeech](https://github.com/xcmyz/FastSpeech)
 - [NVIDIA/waveglow](https://github.com/NVIDIA/waveglow)
 - [KevinMIN95/StyleSpeech](https://github.com/KevinMIN95/StyleSpeech)
+- [jik876/hifi-gan](https://github.com/jik876/hifi-gan)
 
 Paper References:
 
 - [WaveGlow: A Flow-based Generative Network for Speech Synthesis](https://arxiv.org/abs/1811.00002)
 - [Neural Speech Synthesis with Transformer Network Paper](https://arxiv.org/abs/1809.08895)
 - [FastSpeech: Fast, Robust and Controllable Text to Speech](https://arxiv.org/abs/1905.09263)
+- [HiFi-GAN: Generative Adversarial Networks for Efficient and High Fidelity Speech Synthesis](https://arxiv.org/abs/2010.05646)
--- src/tasks/vocoders/base_vocoder.py
+++ src/tasks/vocoders/base_vocoder.py
@@ -1,4 +1,8 @@
-import importlib
+import torch
+from glob import glob
+
+from tasks.base_conversion import BaseConversion
+from tasks.base_lightning import BaseLit
 VOCODERS = {}
 
 
@@ -8,32 +12,39 @@ def register_vocoder(cls):
     return cls
 
 
-def get_vocoder_cls(hparams):
-    if hparams['vocoder'] in VOCODERS:
-        return VOCODERS[hparams['vocoder']]
-    else:
-        vocoder_cls = hparams['vocoder']
-        pkg = ".".join(vocoder_cls.split(".")[:-1])
-        cls_name = vocoder_cls.split(".")[-1]
-        vocoder_cls = getattr(importlib.import_module(pkg), cls_name)
-        return vocoder_cls
+def get_vocoder_cls(config):
+    if config['vocoder'] in VOCODERS:
+        return VOCODERS[config['vocoder']]
+    name = config['vocoder']
+    raise ValueError("Vocoder {name} is not a recognized vocoder.")
 
 
-class BaseVocoder:
-    def spec2wav(self, mel):
-        """
+class BaseVocoder(BaseConversion, BaseLit):
+    def __init__(self, config):
+        super(self).__init__(config)
 
-        :param mel: [T, 80]
-        :return: wav: [T']
-        """
+        self.train_loader = None
+        self.valid_loader = None
+        self.test_loader = None
 
-        raise NotImplementedError
+        if not self.trainable and self.load_ckpt:
+            self.model = get_vocoder_cls(config).load_from_checkpoint(self.ckpt)
+        else:
+            self.model = get_vocoder_cls(config)(config)
 
-    @staticmethod
-    def wav2spec(wav_fn):
-        """
-
-        :param wav_fn: str
-        :return: wav, mel: [T, 80]
-        """
-        raise NotImplementedError
+    def convert(self, phonemes):
+        self.model.eval()
+        with torch.no_grad():
+            output = self.model(phonemes)
+        return output
+    
+    def get_ckpt(self, config):
+        ckpt = config.get("ckpt")
+        if ckpt is None:
+            model_name = get_vocoder_cls(config).__name__
+            print(self.ckpt_dir + model_name)
+            ckpts = sorted(glob(self.ckpt_dir + model_name + "*.pt"), reverse=True) # this might not actually yeild the latest checkpoint
+            if len(ckpts) > 0:
+                ckpt = ckpts[0]
+        
+        return ckpt
